3c6536f081e79a05f03847f0c6e31dcc483a3f32
vhavlena <vohavlena@gmail.com>
2019-02-11 21:45:35 +0800

    BA: Rabit format parsing

--- src/AuxFunctions.hs
+++ src/AuxFunctions.hs
@@ -5,6 +5,7 @@ module AuxFunctions where
 import Data.List
 import Data.Char
 import qualified Data.Set as Set
+import qualified Data.Map as Map
 
 
 printList :: (Show a) => String -> [a] -> String
@@ -21,3 +22,7 @@ printSet delim st = printList delim (Set.toList st)
 
 printSetCom :: (Show a) => Set.Set a -> String
 printSetCom = printSet ","
+
+
+mapSetFromList :: (Ord k, Ord v) => [(k,v)] -> Map.Map k (Set.Set v)
+mapSetFromList = Map.fromListWith (Set.union) . map (\(x,y) -> (x, Set.singleton y))
---
+++ src/RabitAutomataParser.hs
@@ -0,0 +1,99 @@
+{-|
+Module      : RabitAutomataParser
+Description : Rabit automata parser
+Author      : Vojtech Havlena, February 2019
+License     : GPL-3
+-}
+
+module RabitAutomataParser where
+
+import Control.Applicative ((<*))
+
+import Data.List
+
+import Text.Parsec
+import Text.Parsec.Char
+import Text.Parsec.Expr
+import Text.Parsec.Language
+import Text.Parsec.Prim
+import Text.Parsec.String
+import Text.Parsec.Token
+import qualified Data.Set as Set
+
+import BuchiAutomaton
+import qualified AuxFunctions as Aux
+
+--------------------------------------------------------------------------------------------------------------
+-- Part with the data types
+--------------------------------------------------------------------------------------------------------------
+
+type RabitState = String
+type RabitSymbol = String
+
+type RabitTransiton = ((RabitState, RabitSymbol), RabitState)
+
+type RabitBuchiAutomaton = BuchiAutomaton RabitState RabitSymbol
+
+
+--------------------------------------------------------------------------------------------------------------
+-- Part with the definitions of lexemes
+--------------------------------------------------------------------------------------------------------------
+
+lexer = makeTokenParser emptyDef
+m_comma = lexeme lexer (char ',')
+m_lparen = lexeme lexer (char '(')
+m_rparen = lexeme lexer (char ')')
+m_ddot = lexeme lexer (char ':')
+m_to = lexeme lexer (string "->")
+m_quot = lexeme lexer (char '\'')
+m_white = whiteSpace lexer
+m_state = lexeme lexer (many1 (noneOf " \r\n,->"))
+m_label = lexeme lexer (many1 (noneOf " \r\n,->"))
+
+parseFile :: FilePath -> IO RabitBuchiAutomaton
+parseFile filename = do
+  str <- readFile filename
+  return $ parseString str
+
+
+parseString :: String -> RabitBuchiAutomaton
+parseString str = case (parse parseRabitBuchiAutomaton "RabitBuchiAutomataParser" str) of
+  Left err   -> error $ show err
+  Right res  -> res
+
+
+--------------------------------------------------------------------------------------------------------------
+-- Part with the definitions of parsing rules
+--------------------------------------------------------------------------------------------------------------
+
+
+parseState :: Parser RabitState
+parseState = do
+  state <- m_state
+  return state
+
+
+parseTransition :: Parser RabitTransiton
+parseTransition = do
+  symbol <- m_label
+  m_comma
+  from <- m_state
+  m_to
+  to <- m_state
+  return ((from, symbol), to)
+
+
+parseRabitBuchiAutomaton :: Parser RabitBuchiAutomaton
+parseRabitBuchiAutomaton = do
+  ini <- sepEndBy (parseState) newline
+  trans <- many (try parseTransition)
+  fin <- sepBy (parseState) newline
+  let iniSet = Set.fromList ini
+      finSet = Set.fromList fin
+      stSet = Set.union (getAllStates trans) (Set.union iniSet finSet)
+      transMap = Aux.mapSetFromList trans
+  return $ BuchiAutomaton stSet iniSet finSet transMap
+
+
+getAllStates :: [RabitTransiton] -> Set.Set RabitState
+getAllStates = Set.fromList . concat . map (\((a,_),b) -> [a,b])
