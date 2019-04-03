41f67986f080789c0f72c0ec654c7a250ca0ed26
vhavlena <vohavlena@gmail.com>
2019-04-03 12:40:45 +0200

    Compl: Schewe construction #2

--- src/ComplSchewe.hs
+++ src/ComplSchewe.hs
@@ -1,3 +1,9 @@
+{-|
+Module      : ComplSchewe.hs
+Description : Schewe rank-based complementation
+Author      : Vojtech Havlena, April 2019
+License     : GPL-3
+-}
 
 module ComplSchewe (
   complSchewe
@@ -15,8 +21,19 @@ type SufSchewe a = (Set.Set a, Set.Set a, RankFunc a, Int)
 data StateSchewe a =
   Prefix (Set.Set a)
   | Suffix (SufSchewe a)
-  deriving (Show, Ord)
+  deriving (Show)
 
+
+instance (Eq a) => Eq (StateSchewe a) where
+  (Prefix x) == (Prefix y) = x == y
+  (Suffix x) == (Suffix y) = x == y
+  _ == _ = False
+
+instance (Ord a) => Ord (StateSchewe a) where
+  (Prefix x) <= (Prefix y) = x <= y
+  (Suffix x) <= (Suffix y) = x <= y
+  (Prefix _) <= _ = True
+  _ <= _ = False
 
 -- printRabitStateKV :: StateKV String -> String
 -- printRabitStateKV (s, o, f) = "({" ++ (Aux.printSetComF (id) s) ++ "},{"
@@ -32,8 +49,12 @@ oddRanks :: (Ord a) => RankFunc a -> Set.Set a
 oddRanks = Set.fromList . Map.keys . Map.filter (odd)
 
 
+rankOf :: (Ord a) => RankFunc a -> Int
+rankOf = maximum . Map.elems
+
+
 allRanks :: (Ord a) => Set.Set a -> Set.Set a -> [a] -> Set.Set (RankFunc a)
-allRanks fin sset states = generateFromConstr fin sset con where
+allRanks fin sset states = generateSRanksFromConstr fin sset con where
   n = length states
   con = [(q, 2*n) | q <- states]
 
@@ -45,9 +66,12 @@ isRankSTight st f = (odd mRank) && (Set.isSubsetOf odds ranks) where
   odds = Set.fromList [x | x <- [1..mRank], odd x]
 
 
+rankImage :: (Ord a) => Int -> RankFunc a -> Set.Set a
+rankImage i = Map.keysSet . Map.filterWithKey (\_ v -> v == i)
 
-generateFromConstr :: (Ord a) => Set.Set a -> Set.Set a -> [(a, Int)] -> Set.Set (RankFunc a)
-generateFromConstr fin sset = Set.filter (isRankSTight sset) . Set.fromList . map (Map.fromList) . sequence . map (smaller)
+
+generateSRanksFromConstr :: (Ord a) => Set.Set a -> Set.Set a -> [(a, Int)] -> Set.Set (RankFunc a)
+generateSRanksFromConstr fin sset = Set.filter (isRankSTight sset) . Set.fromList . map (Map.fromList) . sequence . map (smaller)
   where
     smaller (x,y)
       | Set.member x fin = [(x,y') | y' <- [0..y], even y']
@@ -56,33 +80,32 @@ generateFromConstr fin sset = Set.filter (isRankSTight sset) . Set.fromList . ma
 
 generateRanking :: (Ord a, Ord b) => Set.Set a -> RankFunc a -> Set.Set a -> b
   -> Transitions a b -> Set.Set (RankFunc a)
-generateRanking fin f act sym  tr = generateFromConstr fin act rest where
+generateRanking fin f act sym  tr = generateSRanksFromConstr fin act rest where
   rest = Map.toList $ Map.fromListWith (min)
     [(q', f Map.! q) | q <- Set.toList act, q' <- succTransList q sym tr]
 
 
-isFinKV :: StateSchewe a -> Bool
-isFinKV (Prefix b) = Set.null b
-isFinKV (Suffix (_,b,_,_)) = Set.null b
+isFinSchewe :: StateSchewe a -> Bool
+isFinSchewe (Prefix b) = Set.null b
+isFinSchewe (Suffix (_,b,_,_)) = Set.null b
 
 
-iniKV :: (Ord a, Ord b) => BuchiAutomaton a b -> Set.Set (StateSchewe a)
-iniKV (BuchiAutomaton st ini fin _) = Set.singleton $ Prefix ini
+iniSchewe :: (Ord a, Ord b) => BuchiAutomaton a b -> Set.Set (StateSchewe a)
+iniSchewe (BuchiAutomaton st ini fin _) = Set.singleton $ Prefix ini
 
 
 succSchewe :: (Ord a, Ord b) => BuchiAutomaton a b -> StateSchewe a -> b
   -> Set.Set (StateSchewe a)
-succSchewe (BuchiAutomaton _ _ fin tr) (Prefix sset) sym = Set.union succs1 succs2 where
-  funcs = Set.toList $ generateRanking fin f sset sym tr
+succSchewe (BuchiAutomaton states _ fin tr) (Prefix sset) sym = Set.union succs1 succs2 where
+  funcs = Set.toList $ allRanks fin sset (Set.toList states)
   succs1 = Set.fromList [Suffix (succSet sset sym tr, Set.empty, f', 0) | f' <- funcs]
   succs2 = Set.singleton $ Prefix (succSet sset sym tr)
-succSchewe (BuchiAutomaton _ _ fin tr) (sset, oset, f) sym = Set.fromList succs where
-  funcs = Set.toList $ generateRanking fin f sset sym tr
-  succs = [(succSet sset sym tr,
-    if not $ Set.null oset then Set.difference (succSet oset sym tr) (oddRanks f')
-    else Set.difference (succSet sset sym tr) (oddRanks f'),
-    f') | f' <- funcs]
+succSchewe (BuchiAutomaton _ _ fin tr) (Suffix (sset, oset, f, i)) sym = Set.fromList succs where
+  funcs = filter (\x -> (rankOf x) == (rankOf f)) $ Set.toList $ generateRanking fin f sset sym tr
+  nsset = succSet sset sym tr
+  succs = if not $ Set.null oset then [Suffix (nsset, Set.intersection (succSet oset sym tr) (rankImage i f'), f', i) | f' <- funcs]
+          else [Suffix (nsset, (rankImage i f'), f', (i+2) `mod` ((rankOf f) + 1)) | f' <- funcs]
 
 
-complKV :: (Ord a, Ord b) => BuchiAutomaton a b -> [b] -> BuchiAutomaton (StateKV a) b
-complKV orig alp = constrFromOrig alp (succSchewe orig) (iniKV orig) (isFinKV)
+complSchewe :: (Ord a, Ord b) => BuchiAutomaton a b -> [b] -> BuchiAutomaton (StateSchewe a) b
+complSchewe orig alp = constrFromOrig alp (succSchewe orig) (iniSchewe orig) (isFinSchewe)
