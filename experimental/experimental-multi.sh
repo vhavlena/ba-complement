9100c0fb638175653ba562dfee1ada18cb0ede5a
vhavlena <vohavlena@gmail.com>
2019-06-18 20:35:57 +0200

    Experimental: settings

--- experimental/experimental-multi.sh
+++ experimental/experimental-multi.sh
@@ -2,7 +2,29 @@
 
 folders=("small" "small2" "small3" "small4" "medium")
 
+echo "Delayed"
+cp ../experimental/settings/del.hs ComplConfig.hs
+make release
 for item in ${folders[*]}
 do
+    echo ${item}
+    python3 ../experimental/experimental.py ./Main ../experiments/${item}/ >> ../results/
+done
+
+echo "Direct"
+cp ../experimental/settings/dir.hs ComplConfig.hs
+make release
+for item in ${folders[*]}
+do
+    echo ${item}
+    python3 ../experimental/experimental.py ./Main ../experiments/${item}/ >> ../results/
+done
+
+echo "Combination"
+cp ../experimental/settings/comb.hs ComplConfig.hs
+make release
+for item in ${folders[*]}
+do
+    echo ${item}
     python3 ../experimental/experimental.py ./Main ../experiments/${item}/ >> ../results/
 done
---
+++ experimental/settings/del.hs
@@ -0,0 +1,18 @@
+{-|
+  Module      : ComplConfig.hs
+  Description : Complementation config file
+  Author      : Vojtech Havlena, June 2019
+  License     : GPL-3
+-}
+
+module ComplConfig where
+
+import ComplSimSchewe
+
+
+tmpFileSkeleton = "tempfa2d3e-ds1.ba"
+tmpFileSimulation = "tempfa2d3e_ds3.ba"
+defaultOutName = "out.ba"
+defaultOutNameGoal = "out.gff"
+remOpt = OptDel
+cfCheckCorrectness = False
